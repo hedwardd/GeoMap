1ed4e94105d3c10433249b4a1e545d3c2fe6cc09
Heath Daniel <mr.heath.daniel@gmail.com>
2021-09-03 22:24:27 -0400

    Display device orientation to screen

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,51 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
+import { DeviceMotion } from 'expo-sensors';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { Subscription } from '@unimodules/core';
+
+DeviceMotion.setUpdateInterval(100);
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const [data, setData] = useState({
+    alpha: 0,
+    beta: 0,
+    gamma: 0,
+  });
+  const [subscription, setSubscription] = useState<Subscription | null>(null);
+
+  const _subscribe = () => {
+    setSubscription(
+      DeviceMotion.addListener(({ rotation }) => {
+        setData(rotation);
+      })
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
+  const { alpha, beta, gamma } = data;
+  const alphaRounded = round(alpha),
+    betaRounded = round(beta),
+    gammaRounded = round(gamma);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>alpha: {alphaRounded}</Text>
+      <Text style={styles.title}>beta: {betaRounded}</Text>
+      <Text style={styles.title}>gamma: {gammaRounded}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
 }
@@ -31,3 +66,11 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
+const round = (n: number | undefined) => {
+  if (!n) {
+    return 0;
+  }
+
+  return Math.floor(n * 100) / 100;
+}
